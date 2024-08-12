463140651758b6d7c6d15101fb4a99d7f5eb32b0
Harsh Verma <hv41096@gmail.com>
2024-08-13 00:34:35 +0530

    styling

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -1,7 +1,7 @@
-import  { useState, useEffect } from 'react';
-import axios from 'axios';
-import Table from '../components/Table';
-import Loader from '../components/Loader';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Table from "../components/Table";
+import Loader from "../components/Loader";
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 interface PortalItem {
   id: number;
@@ -18,11 +18,11 @@ const Admin = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`${BASE_URL}/api/banners`); 
+        const response = await axios.get(`${BASE_URL}/api/banners`);
         console.log(response.data);
         setItems(response.data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       } finally {
         setLoading(false);
       }
@@ -32,27 +32,39 @@ const Admin = () => {
   }, []);
 
   const toggleActive = async (id: number, visible: boolean) => {
-    const response = await axios.post(`${BASE_URL}/api/banners/${id}/toggle-visibility`, {visible}); 
-    if(response.data.message){
-      setItems(items.map(item => 
-        item.id === id ? { ...item, visible: !item.visible } : item
-      ));
+    const response = await axios.post(
+      `${BASE_URL}/api/banners/${id}/toggle-visibility`,
+      { visible }
+    );
+    if (response.data.message) {
+      setItems(
+        items.map((item) =>
+          item.id === id ? { ...item, visible: !item.visible } : item
+        )
+      );
     }
-    
   };
 
   const handleUpdate = (id: number) => {
     console.log(id);
-  };  
+  };
 
   return (
     <div className="min-h-screen bg-black flex justify-center items-center">
-      {loading ? <Loader size='lg'/> :  <Table 
-        items={items} 
-        onToggleActive={toggleActive} 
-        onUpdate={handleUpdate} 
-      />}
-     
+      {loading ? (
+        <Loader size="lg" />
+      ) : (
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="text-3xl font-bold text-gray-300 mb-6 text-center">
+            Available Banners
+          </h2>
+          <Table
+            items={items}
+            onToggleActive={toggleActive}
+            onUpdate={handleUpdate}
+          />
+        </div>
+      )}
     </div>
   );
 };
